1e229eac34abfc848d37e39b57908c118139a07c
Renan Melo <renanrmelo@gmail.com>
2016-12-08 12:22:29 -0200

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,44 @@ require 'vendor/autoload.php';
 use Telegram\Bot\Api;
 use Telegram\Bot\Keyboard\Keyboard;
 use Telegram\Bot\Helpers\Emojify;
-use Telegram\Bot\FileUpload\InputFile;
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 $telegram = new Api();
 
-$inputFile = new InputFile('telegram.png');
+$reply_markup = Keyboard::make();
+$reply_markup->inline();
+$reply_markup->row(
+    Keyboard::inlineButton([
+        'text' => Emojify::text(':see_no_evil:') . ' Será anônimo ' . Emojify::text(':speak_no_evil:'),
+        'callback_data' => '/anonymous sdfds'
+    ])
+);
+$reply_markup->row(
+    Keyboard::inlineButton([
+        'text' => Emojify::text(':bulb:').'Eu',
+        'switch_inline_query_current_chat' => 'deslike'
+    ]),
+    Keyboard::inlineButton([
+        'text' => Emojify::text(':heart:').'Outro',
+        'switch_inline_query' => 'Like!'
+    ]),
+    Keyboard::inlineButton([
+        'text' => Emojify::text(':link:').'PHP',
+        'url' => 'http://php.net'
+    ])
+);
 
-$telegram->sendPhoto([
+$telegram->sendMessage([
     'chat_id' => getenv('CHAT_ID'),
-    //'photo' => $inputFile->open(),
-    'photo' => 'http://php.net/manual/en/images/c0d23d2d6769e53e24a1b3136c064577-php_logo.png',
-    'caption' => 'PHP!!!'
+    'text' =>
+        '<b>bold</b> ' .
+        '<i>italic</i> ' .
+        '<a href="phprio.org">PHPRio</a> ' .
+        '<code>fixed-width</code>' .
+        '<pre>code block</pre>',
+    'parse_mode' => 'HTML',
+    'disable_web_page_preview' => true,
+    'reply_markup' => $reply_markup
 ]);
